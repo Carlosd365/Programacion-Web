0cd2b9d8ded50295e455f64f3916f87bab1a4da9
Carlosd365 <carlitosgoag3@gmail.com>
2025-09-11 21:55:26 -0600

    feat(admin): register Student, Teacher, Course and Grade with useful lists and filters

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Student, Teacher, Course, Grade
 
-# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'enrollment_date')
+    search_fields = ('name', 'email')
+    list_filter = ('enrollment_date',)
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department')
+    search_fields = ('name', 'department')
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'teacher')
+    search_fields = ('title',)
+    list_filter = ('teacher',)
+    filter_horizontal = ('students',)
+
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course', 'score', 'date')
+    search_fields = ('student__name', 'course__title')
+    list_filter = ('date', 'course')
