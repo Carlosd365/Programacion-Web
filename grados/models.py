6b1af54b3bc0bbc1ff876315756297c7a6c7522a
Carlosd365 <carlitosgoag3@gmail.com>
2025-10-07 21:07:34 -0600

    feat(models): define Grado model linked to Curso

--- grados/models.py
+++ grados/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from cursos.models import Curso
 
-# Create your models here.
+class Grado(models.Model):
+    nombre = models.CharField(max_length=50)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
