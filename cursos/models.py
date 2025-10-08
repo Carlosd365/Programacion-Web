bea9b365b8c8abcf7e2fe0dac87fee03b5d2d384
Carlosd365 <carlitosgoag3@gmail.com>
2025-10-07 21:07:01 -0600

    feat(models): add Curso model with foreign key to Maestro

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from maestros.models import Maestro
 
-# Create your models here.
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    maestro = models.ForeignKey(Maestro, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.nombre
