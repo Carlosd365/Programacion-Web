436803cd8df9075d97195332edf095ffc617d564
Carlosd365 <carlitosgoag3@gmail.com>
2025-10-07 21:06:31 -0600

    feat(models): create Maestro model related to Escuela

--- maestros/models.py
+++ maestros/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from escuela.models import Escuela
 
-# Create your models here.
+class Maestro(models.Model):
+    nombre = models.CharField(max_length=100)
+    especialidad = models.CharField(max_length=100)
+    escuela = models.ForeignKey(Escuela, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
