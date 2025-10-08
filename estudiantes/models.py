f9615651965ef68bc25e2c932cdbac80c3a17270
Carlosd365 <carlitosgoag3@gmail.com>
2025-10-07 21:08:01 -0600

    feat(models): add Estudiante model with foreign key to Grado

--- estudiantes/models.py
+++ estudiantes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from grados.models import Grado
 
-# Create your models here.
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.PositiveIntegerField()
+    grado = models.ForeignKey(Grado, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
