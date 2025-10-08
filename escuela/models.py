160571c69b3094e55d6a98b8b3d271bbaed5683f
Carlosd365 <carlitosgoag3@gmail.com>
2025-10-07 21:06:00 -0600

    feat(models): add Escuela model with basic fields

--- escuela/models.py
+++ escuela/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Escuela(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
